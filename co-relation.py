6c37ae32c2ffd92b5a9943d4d6c75d9758e239d9
Maheerr2707 <62205613+Maheerr2707@users.noreply.github.com>
2021-06-22 23:21:29 +0530

    Add files via upload

--- co-relation.py
+++ co-relation.py
@@ -4,13 +4,13 @@ import numpy as np
 
 def getDataSource(data_path):
     marks = []
-    rollnum = []
+    dayspresent = []
     with open(data_path) as f:
         csv_reader = csv.DictReader(f)
         for row in csv_reader:
             marks.append(float(row["Marks In Percentage"]))
-            rollnum.append(float(row["Roll No"]))
-    return{"x":marks,"y":rollnum}
+            dayspresent.append(float(row["Dayspresent"]))
+    return{"x":marks,"y":dayspresent}
 
 def findcorelation(dataSource):
     corelation = np.corrcoef(dataSource["x"],dataSource["y"])
